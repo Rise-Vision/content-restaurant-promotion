2ce19b0121ed5e2791b7c8847d48f99be9fcad34
pcsandford <petersandford1982@gmail.com>
2015-01-23 18:51:48 -0500

    adding pages

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+var sheet = document.querySelector('#sheet');
+
+sheet.addEventListener('google-sheet-data', function(e) {
+  switch (e.detail.type) {
+    case 'rows':
+      document.querySelector('#rows').model = this;
+    break;
+    default:
+    break;
+  }
+});
+
+sheet.addEventListener('core-error', function(e) {
+  alert(e.detail.response);
+});
+
+
+
+});
